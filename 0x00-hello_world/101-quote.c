731e0473d43cfaa75841800a101735d91ed86872
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-13 18:20:55 -0700

    C language introduction project

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 /**
  * main - Entry point of c language code.
- * 
+ *
  * Description: prints without using printf af puts function.
  * Return: Always return (0)
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\n" - Dora Korpar, 2015-10-19"));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
