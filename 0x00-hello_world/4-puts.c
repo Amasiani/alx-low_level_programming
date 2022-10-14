01570a8ca36c1426e8534403643d809e593c096f
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-13 17:44:38 -0700

    C language project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Serves as the entry point for the program
+ *
+ * Description: print using the put() function
+ * Return: if no error otherwise returns non-zero value.
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
