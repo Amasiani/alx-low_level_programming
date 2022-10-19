64e24c6b9a263b0d4a51207bb3fff9f37e09a722
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-19 01:29:53 -0700

    Nested loop function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* jack_baurer - print last hour from 00:00 - 23:59
+* jack_bauer - print last hour from 00:00 - 23:59
 *
 * Return: */
 
-void jack_baurer(void)
+void jack_bauer(void)
 {
 	int hr = 0;
 	int min = 0;
