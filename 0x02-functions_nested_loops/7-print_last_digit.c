d0ebf226e47e56be4cf905d181067939203a27eb
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-18 22:16:04 -0700

    Nested loot function task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,10 @@
 int print_last_digit(int num)
 {
 	int last_digit;
-	last_digit  = num % 10;
+	last_digit = num % 10;
 
-	if (last_digit < 0`)
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef main_h
 #define main_h
-extern int_putchar(char c);
+extern int _putchar(char c);
 extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
 extern int _islower(int c);
@@ -11,6 +11,6 @@ extern int print_last_digit(int);
 extern void jack_baurer(void);
 extern void times_table(void);
 extern int add(int, int);
-extern void (print_to_98(int n);
+extern void print_to_98(int n);
 extern void print_times_table(int n);
 #endif
