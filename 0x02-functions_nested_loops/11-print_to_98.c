77eed900c6a4260adc74eb10bf02c1a42a9e22f6
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-19 02:07:15 -0700

    Nested loop function task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* print_to_9 - print 0 - 98
+* print_to_98 - print 0 - 98
 * @n: value to check for
 *
 * Result: Always 0
@@ -21,11 +21,11 @@ void print_to_98(int n)
 	{
 		for (; n > 98; n--)
 		{
-			printf("%d,  ", n);
+			printf("%d, ", n);
 		}
 	}
 	if (n == 98)
 	{
-		printf("%d\n, ", n);
+		printf("%d\n", n);
 	}
 }
