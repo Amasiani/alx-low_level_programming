61c18f941b6a39d9baa22424d279323427a9466f
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-18 22:44:31 -0700

    Nested loop functionn task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
 		_putchar('\n');
 	}
