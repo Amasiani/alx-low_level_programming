90668c6ce8093c2e849fd704673fc5d38dec0878
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-19 00:01:03 -0700

    Nested loop function task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,4 +5,21 @@
 *
 * Return: */
 
+void jack_bauer(void)
+{
+	int hr = 0;
+	int min = 0;
 
+	while (hr <= 23)
+	{
+		while (min <= 59)
+		{
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			min++
+		}
+		hr++
+}
