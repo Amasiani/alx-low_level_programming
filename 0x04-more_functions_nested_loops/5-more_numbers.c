5823fe7db6b4902e16acdb3be2871ebc5f2ec4dc
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-21 01:57:29 -0700

    print 10 times number between 0 - 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ void more_numbers(void)
 	int count = 0;
 	int num;
 
-	while (count < 15)
+	while (count < 10)
 	{
-		for (num = 0; num < 14; num++)
+		for (num = 0; num < 15; num++)
 		{
-			if (num > 9)
+			if (num >= 10)
 				_putchar((num / 10) + '0');
 			_putchar((num % 10) + '0');
 		}
