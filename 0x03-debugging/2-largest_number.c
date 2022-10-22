2eef909cfbb38148486a337dbd11083eda9cd66e
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-21 21:24:36 -0700

    Fixing largest number function

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+i#include "main.h"
+
+/**
+* largest_number - print largest of 3 numbers
+* @a: first integer
+* @b: secind integer
+* @c: third integer
+* Return: largest number
+*/
+
+int largest(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a < c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	
+	return (largest);
+}
