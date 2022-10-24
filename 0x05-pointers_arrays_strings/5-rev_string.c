fdb2e2f9bede38d74eccc7181d25f3af6bf856fc
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-24 09:10:56 -0700

    print string in reserver order

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string
+* @s: value to check
+* Return: newline
+*/
+
+void rev_string(char *s)
+{
+	char reverse = s[0];
+	int i;
+	int counter = 0;
+
+	while (s[counter] != '\n')
+		counter++;
+		for (i = 0; i < counter; i++)
+		{
+			counter--;
+			reverse = s[i];
+			s[i] ==s[counter];
+		}
+}
