31ddaf4f74a2968cbca96af94801f2f97c6e51ca
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-24 09:30:50 -0700

    print string every other character followed by a newline

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts2 - print every other string, starting with the furst character
+* @s: value to check for
+*/
+
+void puts()char *str
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		for ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
