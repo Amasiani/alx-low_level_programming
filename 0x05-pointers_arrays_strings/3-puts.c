f04ce1882be3de14ef056d67c660a98b60c1cb8a
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-24 08:41:19 -0700

    print sring folowed by a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* puts - print a string followed by newline
+* @str: value to check
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
