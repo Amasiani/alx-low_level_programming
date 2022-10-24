eb7049fbb7703f4d466bdc2f8dc2c3b2fd2a7941
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-24 08:48:53 -0700

    print string in reverse order and folowed newline

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_rev - print string in reverse followed by newline
+* @s: value to look for
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
