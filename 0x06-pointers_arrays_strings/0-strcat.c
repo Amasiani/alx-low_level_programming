d77dfb601f2cb7d4f0b57f82a5752aaffdce8e54
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-26 01:15:19 -0700

    concat string from src to dest

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* *_strcat - concat two string overwriting the null byte
+* and add the null byte in the new string
+* @desk: destination string
+* @src: source string
+*
+* Return: Reutrn s pointer to resulting string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (desc[len])
+	{
+		len++;
+	}
+	if (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
