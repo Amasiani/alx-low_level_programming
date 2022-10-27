61b569d09d77cc7973e200f152c084d14be4f2d9
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-27 11:15:14 -0700

    Readme file explaining the task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* cap-string - capitalize first letter
+* @s: Value to check
+* Return: character
+*/
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (i == 0 && (*(s + i) >= 97 && *(s + i) <= 122))
+		{
+			*(s + i) = *(s + i) - 32;
+			i++;
+		}
+		if (*(s + i) == 32 || *(s + i) == '\n' || *(s + i) == '\t' || *(s + i) == ',' || *(s + i) == ';' || *(s + i) == '!' *(s + i) == '?' *(s + i) == '"' *(s + i) == '(' *(s + i) == ')' *(s + i) == '{' || *(s + i) == '}' *(s + i) == '.')
+		{
+			i++;
+			if (*(s + i) >= 97 && *(s + i) <= 122)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+				i++;
+		}
+	}
+	return (s);
+}
