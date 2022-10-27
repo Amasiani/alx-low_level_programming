a517affae91994a5644a1498899ab0adcaf365c9
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-27 14:26:58 -0700

    convert string to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,11 @@ char *string_toupper(char *ch)
 {
 	int i = 0;
 
-	while (*(ch + i) != '\0')
-	(
-		if (*(ch + i) >= 97 && *(ch + i) <= 122)
-			*(ch + i) = *(ch + i) - 32;
+	while (ch[i])
+	{
+		if (ch[i] >= 97 && ch[i] <= 122)
+			ch[i] -= 32;
 		i++;
-	)
+	}
 	return (ch);
 }
