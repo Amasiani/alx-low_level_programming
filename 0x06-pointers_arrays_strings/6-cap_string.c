17669117b821569a1610a9283af6d1610bcbc2ce
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-28 22:59:17 -0700

    Capitalize all word in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@
 
 char *cap_string(char *s)
 {
-	char counter = 0, i;
+	int counter = 0, i;
 	int sel_word[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + counter) >= 97 && *(s + counter) <= 122)
