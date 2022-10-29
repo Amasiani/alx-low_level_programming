3248d5075ec4096df0036d5ae48281a4f28c5af4
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-29 00:20:28 -0700

    print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer
+* @n: value to check for
+* Return: Always 0
+*/
+
+void print_number(int n)
+{
+	unsigned int i, j, counter;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+
+	j = i;
+	counter = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		counter *= 10;
+	}
+
+	for (; counter >= 1; counter /= 10;)
+	{
+		_putchar(((i / counter) % 10) + 48);
+	}
+}
