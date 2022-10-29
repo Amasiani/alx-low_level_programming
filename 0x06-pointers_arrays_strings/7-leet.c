399a437e7301708edc4e4c41bb51439fb6a2598b
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-28 23:44:51 -0700

    Encode a string 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,7 +11,7 @@ char *leet(char *s)
 	int counter = 0, i;
 	int low_letter[] = {97, 101, 111, 116, 108};
 	int upper_letter[] = {65, 69, 79, 84, 76};
-	int number[] = {52, 51, 48, 55, 49}
+	int number[] = {52, 51, 48, 55, 49};
 
 	while (*(s + counter) != '\0')
 	{
