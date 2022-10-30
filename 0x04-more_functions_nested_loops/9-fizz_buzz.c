d6b82b6f9f291d62c7b01950d994b903ee13d080
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-29 21:51:26 -0700

    prints Buzz or Fizzy for each multiple of 3 or 5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,24 @@
 
 int main(void)
 {
-int count, num;
+int num;
 
-for (count = 1; count <= 100; count++)
+num = 1;
+printf("%d", num);
+
+for (num = 1; num <= 100; num++)
 {
 	if (num % 3 == 0 && num % 5 == 0)
 	{
-		printf("FizzBuzz ");
+		printf(" FizzBuzz");
 	}
 	else if (num % 3 == 0 && num % 5 != 0)
 	{
-		printf("Fizz ");
+		printf(" Fizz");
 	}
 	else if (num % 5 == 0 && num % 3 != 0)
 	{
-		printf("Buzz ");
+		printf(" Buzz");
 	}
 	else
 	{
