d0956f93aeb724faabf3bd1d7b0cfab9218bd12a
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-29 23:18:14 -0700

    print prime numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,32 +9,27 @@
 
 void print_number(int n)
 {
-	unsigned int num1, num2;
-	int i;
-	int calc = 1;
+	unsigned int num1, num2, counter;
 
 	if (n < 0)
 	{
-		n = n * -1;
-		_putchar('-');
-	}
-	num1 = n;
-	num2 = num;
-	if (num1 > 9)
-	{
-		while (num1 >= 10)
-		{
-			calc = calc * 10;
-			num1 = num1 / 10;
-		}
-		_putchar((num2 / calc) + "0")
-		calc = calc / 10;
-
-		for (i = calc; i >= 1; i = 1 / 10)
-		{
-			_putchar((num2 / i) %  10 + '0');
-		}
+		_putchar(45);
+		n = n* -1;
 	}
 	else
-		_putchar(num + '0')
+		num1 = n;
+
+	num2 = num1;
+	counter = 1;
+
+	while (num2 > 9)
+	{
+		num2 /= 10;
+		counter *= 10;
+	}
+
+	for (; counter >= 1; counter /= 10)
+	{
+		_putchar(((num1 / counter) % 10) + 48)
+	}
 }
