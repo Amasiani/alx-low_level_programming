d2be6309d2d5c3dbfbe364ccbde38028cf23dafb
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-10-31 21:52:13 -0700

    locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strchr -  Locates a character ijn a sting
+*
+* @s: string to receive and return
+* @c: Character to receive
+* Return: Character
+*/
+
+char *_strchr(char *s, char c)
+{
+	int counter = 0;
+
+	while (*(s + counter)  != '\0')
+	{
+		if (*(s + counter)  == c)
+			return (s);
+		else
+			_putchar('not found!!');
+		counter++;
+	}
+		return (0);
+}
