d1ddf2310df6dda6b391a41fc168d7c6b8b1fe28
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-01 15:11:06 -0700

    gets the length of a prefix string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,14 +12,23 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int cont = 0;
 	int i = 0;
+	int j = 0;
 	int flag;
 
-	while (*s != '\0')
+	while (*(s + i) != '\0')
 	{
 		flag = 0;
 		while (*(accept + j) != '\0')
 		{
-
+			if (*s == *(acceptt +j))
+				flag = 1;
+			j += 1;
 		}
+		j = 0;
+		if (flag == 0)
+			break;
+		cont += 1;
+		s++
 	}
+	return (cont);
 }
