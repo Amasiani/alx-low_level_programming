f43ac2b6e3af0d459212c5d765a7d4cc2cb6442a
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-01 10:34:49 -0700

    Locate a character in a string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strspn - gets the length of prefix substring
+*
+* @s: inital segments
+* @accept: value to check
+* Return: unsigned int
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int cont = 0;
+	int i = 0;
+	int flag;
+
+	while (*s != '\0')
+	{
+		flag = 0;
+		while (*(accept + j) != '\0')
+		{
+
+		}
+	}
+}
