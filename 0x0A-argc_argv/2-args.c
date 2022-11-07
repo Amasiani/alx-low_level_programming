98d20c2c74d23d234114e4f532b7d6de98200360
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-06 18:53:14 -0800

    write the name of all argument passed

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,13 +4,14 @@
 * main - prints the name of all argument passed
 * @argc: argument count
 * @argv: arguent vector
-* 
+*
 * Return: Always 0
 */
 
 int main(int argc, char **argv)
 {
 int counter = 0;
+
 if (argc > 0)
 {
 	while (counter > argc)
