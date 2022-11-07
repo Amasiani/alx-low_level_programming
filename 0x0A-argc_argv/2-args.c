6fb7d1dc702aa2b207560f4d80496246e64362dc
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-06 18:49:19 -0800

    write the name of all argument passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - prints the name of all argument passed
+* @argc: argument count
+* @argv: arguent vector
+* 
+* Return: Always 0
+*/
+
+int main(int argc, char **argv)
+{
+int counter = 0;
+if (argc > 0)
+{
+	while (counter > argc)
+	{
+		printf("%s\n", argv[counter])'
+		counter++;
+	}
+}
+Return (0);
+}
