c87d3891266ddcb92d6e70856bc92055dce7a737
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-08 18:46:04 -0800

    create atwo dimensional array from given integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,12 +16,12 @@ int **alloc_grid(int width, int height)
 
 	if (height <= 0 || width <= 0)
 		return (NULL);
-	array = (int**)malloc(sizeof(int*) * height);
+	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		array[i] = (int*)malloc(sizeof(int) * width);
+		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			free(array);
