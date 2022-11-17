025ab0998bdefe853f462fe81b8a7626b3948704
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-17 10:56:57 -0800

    Varidic function that sum the argument passed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	sum = 0;
-	va_start(nums, n)
+	va_start(nums, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(nums, int);
 	va_end(nums);
