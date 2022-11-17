d0431d97d43963f6d18a8256f503bde31c4c84a3
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-16 21:45:13 -0800

    select the correct operator based on user choice

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,6 +11,8 @@
 
 int main(int argc, int **argv)
 {
+char *sign;
+char operator;
 int a, b;
 int (*operation)(int, int);
 
@@ -19,20 +21,22 @@ if (argc != 4)
 	printf("Error\n");
 	exit(98);
 }
-if (argv[2][1])
-{
-	printf("Error\n");
-	exit(99);
-}
-operation = get_op_func(argv[2]);
-if (operation == NULL)
+operator = argv[2][0];
+if ((operator != '*' && operator != '+' && operator != '-' && operator != '/'
+&& operator != '%') || argv[2][1] != '\0')
 {
 	printf("Error\n");
 	exit(99);
 }
 a = atoi(argv[1]);
 b = atoi(argv[3]);
-
+if ((operator == '/' || operator == '%') && b == 0)
+{
+	printf("Error\n");
+	exit(100);
+}
+sign = argv[2];
+operation = get_op_func(sign);
 printf("%d\n", operation(a, b));
 return (0);
 }
