e3c41df6f18106000d1bf3cc8ee5fd30a64234e0
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-17 21:35:47 -0800

    prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,99 @@
+#include "variadic_functions.h"
+
+/**
+* print_int - print integer
+* @list: argument list
+* @separator: string separator between integers
+*
+* Return: Always 0
+*/
+
+void print_integers(va_list list, char *separator)
+{
+	printf("%s%d", separator, va_arg(list, int));
+}
+
+/**
+* print_characters - string charaters
+* @list: argument list
+* @separator: separator between characters
+* 
+* Return: Always 0
+*/
+
+void print_characters(va_list list, char *separator)
+{
+	printf("%s%c", separator, va_arg(list, int));
+}
+
+/**
+* print_strings - prints strings
+* @list: list of arguments
+* @separator: separator between strings
+*
+* Return: Always 0
+*/
+
+void print_strings(va_list list, char *separator)
+{
+	char *st;
+
+	st = va_arg(list , char *);
+	if (st == NULL)
+		printf("%s%s", separator, st = "(nil)");
+}
+
+/**
+* print_floats - prints float
+* @separator: separator between float
+* @list: argument list
+*
+* Return: Always 0
+*/
+
+void print_floats(va_list list, char *separator)
+{
+	printf("%s%f", separator, va_arg(list, double));
+}
+
+
+/**
+* print_all - print anything
+* @format: argument list passed
+*
+* Return: Always 0
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i, j;
+	char *separator;
+
+	type_t ops[] = {
+		{"c", print_characters},
+		{"i", print_integers},
+		{"f", print_floats},
+		{"s", print_strings},
+		{NULL, NULL}
+	};
+	va_start(list, format);
+	i = 0;
+	separator = "";
+	while (format != NULL && format[i] != '\0')
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *(ops[j]).op)
+			{
+				ops[j].f(list, separator);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
