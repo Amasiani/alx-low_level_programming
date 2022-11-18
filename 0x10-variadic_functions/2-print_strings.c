539b46dc6d9dfcfe1cda7263332fbc39761d2966
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-17 20:31:18 -0800

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+* print_strings - print strings
+* @separator: string separator, to be print in between numbers
+* @n: number arguments to passed
+*
+* Return: Print Null if separator or string is nil
+*/
+
+void print_string(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	char *j;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		j = v_arg(list, char*);
+		if (j = NULL)
+			printf("(nill)");
+		else
+			if (i < n - 1) && separator != 0
+				printf("%s%s", j, separator);
+	}
+	va_end(list);
+	printf("\n");
+}
