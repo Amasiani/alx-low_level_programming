a4a10296723dc531f6a9ac06dd3699c7e4ed200c
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-28 05:16:57 -0800

    first commit and function that pint all elements in listin_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+* print_listint - print all elements of listint_t list
+* @h: listint_t list head
+*
+* Return: numbers of elements
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++, h = h->next)
+		printf("%d\n", h->n);
+	return (i);
+}
