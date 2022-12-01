16acc2dd6ccd6ecfff3f3fca76d8beca7c45cb77
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-11-30 21:24:45 -0800

    list ll element in listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,9 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
-	for (i = 0; h != NULL; i++, h = h->next)
+	while (i)
+	{
 		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
 	return (i);
 }
