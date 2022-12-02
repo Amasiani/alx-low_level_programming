bf0519d85963300cf7e7e7beaf78935910cecaa5
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-12-01 18:57:20 -0800

    set the value of a given bit to a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* get_bit - set te value of a bit to 1 at a given index
+* @n: Number base
+* @index: index to get
+*
+* Return: I if it worked else -1
+*/
+
+int get_bit(unsigned int n, unsigned int index)
+{
+	if (index >= 32)
+		return (-1);
+	return ((n >> index) & 1);
+}
