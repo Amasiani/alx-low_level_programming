cd0203800f28d8ecfc6a383a721772e59e5a4725
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-12-04 19:32:00 -0800

    convert a binary nukber to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,10 +22,14 @@ unsigned int binary_to_uint(const char *b)
 		;
 	for (i--, opt = 1; i >= 0; i--, opt *= 2)
 	{
-		if (b[i] != 48 && b[i] != 49)
+		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
 		if (b[i] & 1)
+		{
 			number += opt;
+		}
 	}
 	return (number);
 }
