9a80c6bca769130b09c1ad0bf27b69413d138e57
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-12-05 21:44:42 -0800

    read a file and print the number of letter to the stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* read_textfile - read text file and print the letters
+* @filename: filename
+* @letters: numbr of letters printed
+*
+* Return: number letter printed else 0 if error or nul
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, num;
+	char *buf;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+	nrd = read(fd, buf, letters);
+	num = write(STDOUT_FILEND, buf, nrd);
+
+	close(fd);
+	free(buf);
+	return (num);
+}
