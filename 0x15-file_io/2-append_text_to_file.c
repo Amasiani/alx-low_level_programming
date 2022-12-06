39d591030b6ed1e217f611fc0c6c0f00cc050250
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2022-12-05 22:53:47 -0800

    append text to the ned of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
 * @filename: filename
 * @text_content: added content
 *
-* Return: 1 if file exist else -1 
+* Return: 1 if file exist else -1
 */
 
 int append_text_to_file(const char *filename, char *text_content)
