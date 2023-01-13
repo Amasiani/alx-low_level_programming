0280a9a782c676f730b144ab5792149cca629b2e
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-12 20:44:28 -0800

    print number elements in a dlistint list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -12,7 +12,7 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t elements;
 
-	for (elements = 0; h != NULL; h = h->next; elements++)
+	for (elements = 0; h != NULL; h = h->next, elements++)
 		;
 	return (elements);
 }
