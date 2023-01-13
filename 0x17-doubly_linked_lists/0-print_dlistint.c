fb7abe56b5af58483ece1d07640865e70889c0cf
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-12 20:29:38 -0800

    print dlistint linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* print_distint - Print a doubly linked list
+* print_dlistint - Print a doubly linked list
 *
 * @h: Head of a linked list
 *
@@ -14,7 +14,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t elements;
 
 	for (elements = 0; h != NULL; h = h->next, elements++)
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
 
 	return (elements);
 }
