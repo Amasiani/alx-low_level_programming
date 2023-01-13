434b516896a935200480ac39086bac271c5693db
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-13 13:24:49 -0800

    insert new node at given position

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+* delete_dnodeint_at_index - Deletes the node at the index
+* of dlistint_t linked list
+* @head: Head of the list
+* @index: Index of the node to delete
+*
+* Description: Delete node at a given index
+* Return: 1 on success else -1
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_ *actual = *head;
+	unsigned int cnt;
+
+	if (!head || !actual)
+		return (-1);
+
+	if (index == 0)
+	{
+		actual = actual->next;
+		free(*head);
+		*head = actual;
+		if (*head)
+			(*head)->prev = NULL;
+		return (1);
+	}
+	for (cnt = 0; actual; cnt++)
+	{
+		if (cnt == index)
+		{
+			if (actual->next)
+			{
+				(actual->prev)->next = actual->next;
+				(actual->next)->prev = actual->prev;
+				free(actual);
+				return (1);
+			}
+			else
+			{
+				(actual->prev)->next = NULL;
+				free(actual);
+				return (1);
+			}
+		}
+		actual = actual->next;
+	}
+	return (-1)
+}
