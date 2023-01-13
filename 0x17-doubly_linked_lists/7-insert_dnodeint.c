6c00d86721b89e1a97f48d90aad7d6edbb8ee885
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-13 12:36:44 -0800

    insert new node at given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 * Description: Insert n into a dlistimt_t linked list
 */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t *hh, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *actual;
 	unsigned int i;
