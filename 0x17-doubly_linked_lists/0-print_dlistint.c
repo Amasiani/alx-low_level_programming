1de5f41da62734add71f2cf5ef3547b2345d7049
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-12 20:40:12 -0800

    print dlistint linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,13 +2,11 @@
 
 /**
 * print_dlistint - Print a doubly linked list
-*
 * @h: Head of a linked list
 *
 * Return: Number of elements of the list
 * Description: Print all the elements of a linked list
 */
-i
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t elements;
