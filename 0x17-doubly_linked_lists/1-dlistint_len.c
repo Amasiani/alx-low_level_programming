652135eacb4645d7e34e04a4fbf0b70f137cf6a4
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-12 20:20:12 -0800

    size_t dlistint_len task

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* dlistint_len - Number of elements ina linked dlistint_ list
+* @h: Head of the linked list
+*
+* Description: Print number of eements in a linked dlistint_t link
+* Return: Number of elements
+*/
+
+size_t dlistint_len(dlistint_t *h)
+{
+	size_t elements;
+
+	for (elements =0; h != NULL; h = h->next; elements++)
+		;
+	return (elements);
+}
