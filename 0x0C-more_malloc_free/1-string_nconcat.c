56a3b3ca2aee1efeeec247b8a3af85a23074926b
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-29 20:59:08 -0800

    1-string_nconcat corrected

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,9 +24,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		len1++;
 	len2 = 0;
-	for (len2 = 0; *(s2 + i) != '\0'; i++)
+	for (i = 0; *(s2 + i) != '\0'; i++)
 		len2++;
-	if (n > len2)
+	if (n >= len2)
 		n = len2;
 	length = len1 + n;
 	res = malloc((sizeof(char) * length) + 1);
