91170fc61f0dc2a4166f8a71493b7e8eb0079174
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-29 19:46:57 -0800

    100-times_table updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,44 @@
 #include "main.h"
 
 /**
-* print_times_table - print n times table.
-* @n: value to check.
-*
-* Return: Always 0
-*/
-
+ * print_times_table - Prints the n times table
+ *
+ * @n: number times table (0 < n <= 15)
+ *
+ * Return: no return
+ */
 void print_times_table(int n)
 {
-	int col, row;
-	int multiple = 0;
+	int a, b, op;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <= 15)
 	{
-		for (row = 0; row <= n; row++)
+		for (a = 0; a <= n; a++)
 		{
-			for (col = 0; col <= n; col++)
+			_putchar(48);
+			for (b = 1; b <= n; b++)
 			{
-				multiple = (row * col);
-				if (multiple < 10)
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (op <= 9)
 				{
-					if (col !=  0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar(multiple + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
 				}
-				else if (multiple < 100)
+				else if (op <= 99)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((multiple / 10) + '0');
-					_putchar((multiple % 10) + '0');
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
 				}
 				else
 				{
-					_putchar(' ');
-					_putchar((multiple / 100) + '0');
-					_putchar(((multiple % 100) / 10) + '0');
-					_putchar((multiple % 10) + '0');
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
 				}
-				if (col != n)
-					_putchar(',');
 			}
 			_putchar('\n');
 		}
